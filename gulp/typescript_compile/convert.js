051674485e5c67c5121c503c43d837ee2f0bda9f
yyc <395976266@qq.com>
2017-02-09 17:54:05 +0800

    feat: write script(invoke typescript compile api) to convert namespace to es2015

---
+++ gulp/typescript_compile/convert.js
@@ -0,0 +1,304 @@
+"use strict";
+var ts = require("typescript");
+// import * as fs from "fs";
+var path = require("path");
+//todo refactor
+var fs = require("fs-extra");
+;
+;
+;
+;
+;
+;
+var fileDataMap = {};
+var exportDataMap = {};
+var sourceFileMap = {};
+var hasConfilctImportData = false;
+var confilctImportDataMap = {};
+var ArrayUtils = (function () {
+    function ArrayUtils() {
+    }
+    ArrayUtils.removeRepeatItems = function (arr) {
+        var resultArr = [], self = this;
+        arr.forEach(function (ele) {
+            if (self.contain(resultArr, ele)) {
+                return;
+            }
+            resultArr.push(ele);
+        });
+        return resultArr;
+    };
+    ArrayUtils.contain = function (arr, ele) {
+        for (var i = 0, len = arr.length; i < len; i++) {
+            var value = arr[i];
+            if (ele === value || (value.contain && value.contain(ele))) {
+                return true;
+            }
+        }
+        return false;
+    };
+    ;
+    return ArrayUtils;
+}());
+function generateDocumentation(rootDir, sourceFileGlobArr, destDir, options) {
+    // import * as glob from "glob";
+    var glob = require("glob");
+    for (var _i = 0, sourceFileGlobArr_1 = sourceFileGlobArr; _i < sourceFileGlobArr_1.length; _i++) {
+        var filePath = sourceFileGlobArr_1[_i];
+        var globCwd = "/", fileAbsolutePaths = glob.sync(path.join(rootDir, filePath), {
+            cwd: globCwd
+        });
+        _getAllSourceFiles(fileAbsolutePaths);
+        var _loop_1 = function (filePath_1) {
+            if (sourceFileMap.hasOwnProperty(filePath_1)) {
+                var sourceFile = sourceFileMap[filePath_1];
+                var textLineArr = sourceFile.text.split('\n');
+                var moduleBlockContent = null;
+                var findLastBraceIndex = function (textLineArr) {
+                    for (var i = textLineArr.length - 1; i >= 0; i--) {
+                        if (textLineArr[i] === "}") {
+                            return i;
+                        }
+                    }
+                    throw new Error("} not exist in " + filePath_1);
+                };
+                var findModuleDeclarationIndex = function (textLineArr) {
+                    for (var i = textLineArr.length - 1; i >= 0; i--) {
+                        if (/\s*module\s[\w\d]+\s*\{/.test(textLineArr[i])) {
+                            return i;
+                        }
+                    }
+                    throw new Error("module declaration not exist in " + filePath_1);
+                };
+                var moduleDeclarationIndex = findModuleDeclarationIndex(textLineArr);
+                if (moduleDeclarationIndex > 0) {
+                    moduleBlockContent = textLineArr.slice(0).slice(0, moduleDeclarationIndex).join('\n');
+                }
+                else {
+                    moduleBlockContent = "";
+                }
+                moduleBlockContent += textLineArr.slice(moduleDeclarationIndex + 1, findLastBraceIndex(textLineArr)).join('\n');
+                var fileData = {};
+                fileData["moduleBlockContent"] = moduleBlockContent;
+                var exportData_1 = [];
+                ts.forEachChild(sourceFile, function (node) {
+                    _visitExportData(node, exportData_1, filePath_1);
+                });
+                fileData["exportData"] = {
+                    nameArr: ArrayUtils.removeRepeatItems(exportData_1)
+                };
+                fileDataMap[filePath_1] = fileData;
+            }
+        };
+        for (var filePath_1 in sourceFileMap) {
+            _loop_1(filePath_1);
+        }
+        var _loop_2 = function (filePath_2) {
+            if (sourceFileMap.hasOwnProperty(filePath_2)) {
+                var sourceFile = sourceFileMap[filePath_2];
+                var importData_1 = {};
+                ts.forEachChild(sourceFile, function (node) {
+                    _visitImportData(node, importData_1, filePath_2);
+                });
+                for (var filePath_3 in importData_1) {
+                    if (importData_1.hasOwnProperty(filePath_3)) {
+                        var nameArr = importData_1[filePath_3];
+                        importData_1[filePath_3] = ArrayUtils.removeRepeatItems(nameArr);
+                    }
+                }
+                var fileData = fileDataMap[filePath_2];
+                fileData["importData"] = importData_1;
+            }
+        };
+        for (var filePath_2 in sourceFileMap) {
+            _loop_2(filePath_2);
+        }
+        _writeES2015Files(rootDir, destDir);
+        _generateIndexFile(fileDataMap, rootDir, destDir);
+        if (hasConfilctImportData) {
+            console.log("hasConfilctImportData! the data is:");
+            console.log(JSON.stringify(confilctImportDataMap));
+        }
+    }
+    // console.log(JSON.stringify(fileDataMap));
+    console.log("finish");
+    return;
+}
+function _getAllSourceFiles(fileAbsolutePaths) {
+    fileAbsolutePaths.forEach(function (filePath) {
+        var sourceFile = ts.createSourceFile(filePath, fs.readFileSync(filePath).toString(), ts.ScriptTarget.ES5);
+        sourceFileMap[filePath] = sourceFile;
+    });
+}
+function _visitExportData(node, exportData, filePath) {
+    if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+        var module_1 = node;
+        if (_isModuleBlock(module_1.body)) {
+            var block = module_1.body;
+            for (var _i = 0, _a = block.statements; _i < _a.length; _i++) {
+                var statement = _a[_i];
+                if (!_isExported(statement)) {
+                    continue;
+                }
+                switch (statement.kind) {
+                    case ts.SyntaxKind.ClassDeclaration:
+                    case ts.SyntaxKind.InterfaceDeclaration:
+                    case ts.SyntaxKind.EnumDeclaration:
+                    case ts.SyntaxKind.FunctionDeclaration:
+                        var name_1 = _getName(statement);
+                        exportData.push(name_1);
+                        _append(exportDataMap, name_1, filePath);
+                        break;
+                    case ts.SyntaxKind.VariableStatement:
+                        for (var _b = 0, _c = statement.declarationList.declarations; _b < _c.length; _b++) {
+                            var n = _c[_b];
+                            var name_2 = _getName(n);
+                            exportData.push(name_2);
+                            _append(exportDataMap, name_2, filePath);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    // if (node.kind === ts.SyntaxKind.ModuleBlock) {
+    // node.statements
+    // }
+}
+function _isModuleBlock(body) {
+    return body.kind === ts.SyntaxKind.ModuleBlock;
+}
+function _append(obj, key, value) {
+    if (_isArray(obj[key])) {
+        obj[key].push(value);
+    }
+    else {
+        obj[key] = [value];
+    }
+}
+function _isArray(arr) {
+    return Object.prototype.toString.call(arr) === "[object Array]";
+}
+function _getName(node) {
+    if (!node.name) {
+        return null;
+    }
+    return node.name.text;
+}
+function _visitImportData(node, importData, filePath) {
+    if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+        var module_2 = node;
+        if (_isModuleBlock(module_2.body)) {
+            var block = module_2.body;
+            ts.forEachChild(block, function (node) {
+                _visit(node, importData, filePath);
+            });
+        }
+    }
+}
+function _visit(node, importData, filePath) {
+    if (!!node.text) {
+        var name_3 = node.text, data = exportDataMap[name_3];
+        if (_isArray(data)) {
+            if (data.length > 1) {
+                _recordConflictImport(name_3, data, filePath);
+            }
+            else {
+                var importedFilePath = data[0];
+                if (importedFilePath !== filePath) {
+                    _append(importData, _getRelativePath(filePath, importedFilePath), name_3);
+                }
+            }
+        }
+    }
+    ts.forEachChild(node, function (node) {
+        _visit(node, importData, filePath);
+    });
+}
+function _getRelativePath(from, to, isDir) {
+    if (isDir === void 0) { isDir = false; }
+    var relativePath = path.relative(isDir ? from : path.dirname(from), to);
+    if (relativePath[0] !== ".") {
+        relativePath = "./" + relativePath;
+    }
+    return relativePath;
+}
+function _recordConflictImport(name, data, filePath) {
+    hasConfilctImportData = true;
+    _append(confilctImportDataMap[filePath], name, data);
+}
+function _writeES2015Files(rootDir, destDir) {
+    for (var filePath in fileDataMap) {
+        if (fileDataMap.hasOwnProperty(filePath)) {
+            var fileData = fileDataMap[filePath];
+            var destFilePath = _getDestFilePath(destDir, rootDir, filePath);
+            // let importData = _mergeImportsWhichComeFromTheSameFile(fileData.importData);
+            var fileContent = fileData.moduleBlockContent;
+            fileContent = _addImport(fileData.importData, fileContent);
+            fs.mkdirsSync(path.dirname(destFilePath));
+            fs.writeFileSync(destFilePath, fileContent);
+        }
+    }
+}
+function _getDestFilePath(destDir, rootDir, filePath) {
+    return path.join(destDir, _getRelativePath(rootDir, filePath, true));
+}
+function _generateIndexFile(fileDataMap, rootDir, destDir) {
+    var indexFilePath = path.join(destDir, "index.ts");
+    if (fs.existsSync(indexFilePath)) {
+        throw new Error("already exist index file:" + indexFilePath + ", can't generate it!");
+    }
+    var content = "";
+    for (var filePath in fileDataMap) {
+        if (fileDataMap.hasOwnProperty(filePath)) {
+            var fileData = fileDataMap[filePath], nameArr = fileData.exportData.nameArr;
+            if (nameArr.length === 0) {
+                continue;
+            }
+            content += "export {" + nameArr + "} from \"" + _getRelativePath(rootDir, filePath, true) + "\";\n";
+        }
+    }
+    fs.writeFileSync(indexFilePath, content);
+}
+function _addImport(importData, fileContent) {
+    var importContent = "";
+    for (var filePath in importData) {
+        if (importData.hasOwnProperty(filePath)) {
+            var nameArr = importData[filePath];
+            importContent += "import {" + nameArr.join(',') + "} from \"" + filePath.replace(".ts", "") + "\";\n";
+        }
+    }
+    if (importContent.length > 0) {
+        return importContent + "\n" + fileContent;
+    }
+    return fileContent;
+}
+function _isExported(node) {
+    // return (node.flags & (<any>ts.NodeFlags).Export) !== 0 || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
+    return (node.flags === ts.NodeFlags.Export)
+        || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
+}
+//todo move to wonder-package
+function parseOption(name) {
+    var value = null, i = process.argv.indexOf(name);
+    if (i > -1) {
+        value = process.argv[i + 1];
+    }
+    return value;
+}
+// function isDefinOption(name) {
+//     return process.argv.indexOf(name) > -1;
+// }
+//
+// module.exports = {
+//     parseOption: parseOption,
+//     isDefinOption: isDefinOption
+// }
+var rootDir = parseOption("--rootDir"), sourceFileGlobArr = parseOption("--sourceFileGlob").split(','), destDir = parseOption("--destDir") || "./dest/";
+fs.removeSync(destDir);
+generateDocumentation(rootDir, sourceFileGlobArr, destDir, {
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.System
+});
---
+++ gulp/typescript_compile/convert.ts
@@ -0,0 +1,428 @@
+import * as ts from "typescript";
+import * as path from "path";
+
+//todo refactor
+var fs = require("fs-extra");
+
+
+interface IFileData {
+    importData: IImportData;
+    exportData: IExportData;
+    moduleBlockContent: string;
+}
+
+interface IFileDataMap {
+    [filePath: string]: IFileData
+}
+
+
+interface IImportData {
+    [importFilePath: string]: Array<string>
+}
+
+interface IExportData {
+    nameArr: Array<string>;
+}
+
+
+interface IExportDataMap {
+    [exportTargetName: string]: Array<string>
+}
+
+interface IConfilctImportDataMap {
+    [filePath: string]: {
+        [importTargetName: string]: Array<string>
+    }
+}
+
+var fileDataMap: IFileDataMap = <IFileDataMap>{};
+
+var exportDataMap: IExportDataMap = <IExportDataMap>{};
+
+var sourceFileMap = {};
+
+
+var hasConfilctImportData = false;
+
+var confilctImportDataMap = {};
+
+
+class ArrayUtils {
+    public static removeRepeatItems(arr: Array<any>) {
+        var resultArr = [],
+            self = this;
+
+        arr.forEach(function (ele) {
+            if (self.contain(resultArr, ele)) {
+                return;
+            }
+
+            resultArr.push(ele);
+        });
+
+        return resultArr;
+    }
+
+    public static contain(arr: Array<any>, ele: any) {
+        for (let i = 0, len = arr.length; i < len; i++) {
+            let value = arr[i];
+
+            if (ele === value || (value.contain && value.contain(ele))) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+}
+
+
+function generateDocumentation(rootDir, sourceFileGlobArr, destDir, options: ts.CompilerOptions): void {
+    // import * as glob from "glob";
+    var glob = require("glob");
+
+    for (let filePath of sourceFileGlobArr) {
+        let globCwd = "/",
+            fileAbsolutePaths = glob.sync(path.join(rootDir, filePath), {
+                cwd: globCwd
+            });
+
+        _getAllSourceFiles(fileAbsolutePaths);
+
+
+        for (let filePath in sourceFileMap) {
+            if (sourceFileMap.hasOwnProperty(filePath)) {
+                let sourceFile = sourceFileMap[filePath];
+
+                let textLineArr = sourceFile.text.split('\n');
+
+                let moduleBlockContent = null;
+
+                let findLastBraceIndex = (textLineArr) => {
+                    for (let i = textLineArr.length - 1; i >= 0; i--) {
+                        if (textLineArr[i] === "}") {
+                            return i;
+                        }
+                    }
+
+                    throw new Error(`} not exist in ${filePath}`);
+                };
+
+
+                let findModuleDeclarationIndex = (textLineArr) => {
+                    for (let i = textLineArr.length - 1; i >= 0; i--) {
+                        if (/\s*module\s[\w\d]+\s*\{/.test(textLineArr[i])) {
+                            return i;
+                        }
+                    }
+
+                    throw new Error(`module declaration not exist in ${filePath}`);
+                };
+
+                let moduleDeclarationIndex = findModuleDeclarationIndex(textLineArr);
+
+                if (moduleDeclarationIndex > 0) {
+                    moduleBlockContent = textLineArr.slice(0).slice(0, moduleDeclarationIndex).join('\n');
+                }
+                else {
+                    moduleBlockContent = "";
+                }
+
+                moduleBlockContent += textLineArr.slice(moduleDeclarationIndex + 1, findLastBraceIndex(textLineArr)).join('\n');
+
+
+                let fileData: IFileData = <IFileData>{};
+
+
+                fileData["moduleBlockContent"] = moduleBlockContent;
+
+                let exportData: Array<string> = [];
+
+
+                ts.forEachChild(sourceFile, (node: ts.Node) => {
+                    _visitExportData(node, exportData, filePath)
+                });
+
+                fileData["exportData"] = {
+                    nameArr: ArrayUtils.removeRepeatItems(exportData)
+                };
+
+                fileDataMap[filePath] = fileData;
+            }
+        }
+
+        for (let filePath in sourceFileMap) {
+            if (sourceFileMap.hasOwnProperty(filePath)) {
+                let sourceFile = sourceFileMap[filePath];
+
+                let importData = {};
+
+                ts.forEachChild(sourceFile, (node: ts.Node) => {
+                    _visitImportData(node, importData, filePath);
+                });
+
+                for (let filePath in importData) {
+                    if (importData.hasOwnProperty(filePath)) {
+                        let nameArr = importData[filePath];
+
+                        importData[filePath] = ArrayUtils.removeRepeatItems(nameArr);
+                    }
+                }
+
+
+                let fileData = fileDataMap[filePath];
+
+                fileData["importData"] = importData;
+            }
+        }
+
+        _writeES2015Files(rootDir, destDir);
+
+        _generateIndexFile(fileDataMap, rootDir, destDir);
+
+        if (hasConfilctImportData) {
+            console.log("hasConfilctImportData! the data is:");
+            console.log(JSON.stringify(confilctImportDataMap));
+        }
+    }
+
+
+    // console.log(JSON.stringify(fileDataMap));
+
+
+    console.log("finish");
+
+    return;
+
+}
+
+
+function _getAllSourceFiles(fileAbsolutePaths) {
+    fileAbsolutePaths.forEach((filePath: string) => {
+        let sourceFile = ts.createSourceFile(filePath, fs.readFileSync(filePath).toString(), ts.ScriptTarget.ES5);
+
+        sourceFileMap[filePath] = sourceFile;
+    });
+}
+
+function _visitExportData(node: ts.Node, exportData: Array<string>, filePath) {
+    if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+        let module = <ts.ModuleDeclaration>node;
+
+        if (_isModuleBlock(module.body)) {
+            let block = <ts.ModuleBlock>module.body;
+
+            for (let statement of block.statements) {
+                if (!_isExported(statement)) {
+                    continue;
+                }
+
+                switch (statement.kind) {
+                    case ts.SyntaxKind.ClassDeclaration:
+                    case ts.SyntaxKind.InterfaceDeclaration:
+                    case ts.SyntaxKind.EnumDeclaration:
+                    case ts.SyntaxKind.FunctionDeclaration:
+                        let name = _getName(statement);
+
+                        exportData.push(name);
+
+                        _append(exportDataMap, name, filePath);
+                        break;
+                    case ts.SyntaxKind.VariableStatement:
+                        for (let n of (<ts.VariableStatement>statement).declarationList.declarations) {
+                            let name = _getName(n);
+
+                            exportData.push(name);
+                            _append(exportDataMap, name, filePath);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+function _isModuleBlock(body: ts.Node) {
+    return body.kind === ts.SyntaxKind.ModuleBlock;
+}
+
+function _append(obj, key, value) {
+    if (_isArray(obj[key])) {
+        obj[key].push(value);
+    }
+    else {
+        obj[key] = [value];
+    }
+}
+
+function _isArray(arr: any) {
+    return Object.prototype.toString.call(arr) === "[object Array]";
+}
+
+function _getName(node: any) {
+    if (!node.name) {
+        return null;
+    }
+
+    return node.name.text;
+}
+
+function _visitImportData(node: ts.Node, importData, filePath) {
+    if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+        let module = <ts.ModuleDeclaration>node;
+
+        if (_isModuleBlock(module.body)) {
+            let block = <ts.ModuleBlock>module.body;
+
+            ts.forEachChild(block, (node: ts.Node) => {
+                _visit(node, importData, filePath);
+            });
+        }
+    }
+}
+
+function _visit(node: any, importData, filePath) {
+    if (!!node.text) {
+        let name = node.text,
+            data = exportDataMap[name];
+
+        if (_isArray(data)) {
+            if (data.length > 1) {
+                _recordConflictImport(name, data, filePath);
+            }
+            else {
+                let importedFilePath = data[0];
+
+                if (importedFilePath !== filePath) {
+                    _append(importData, _getRelativePath(filePath, importedFilePath), name);
+                }
+            }
+        }
+    }
+
+    ts.forEachChild(node, (node: ts.Node) => {
+        _visit(node, importData, filePath);
+    });
+}
+
+function _getRelativePath(from, to, isDir: boolean = false) {
+    var relativePath = path.relative(isDir ? from : path.dirname(from), to);
+
+    if (relativePath[0] !== ".") {
+        relativePath = `./${relativePath}`;
+    }
+
+    return relativePath;
+}
+
+function _recordConflictImport(name, data, filePath) {
+    hasConfilctImportData = true;
+    _append(confilctImportDataMap[filePath], name, data);
+}
+
+function _writeES2015Files(rootDir, destDir) {
+    for (let filePath in fileDataMap) {
+        if (fileDataMap.hasOwnProperty(filePath)) {
+            let fileData = fileDataMap[filePath];
+
+            let destFilePath = _getDestFilePath(destDir, rootDir, filePath);
+
+            let fileContent = fileData.moduleBlockContent;
+
+            fileContent = _addImport(fileData.importData, fileContent);
+
+            fs.mkdirsSync(path.dirname(destFilePath));
+            fs.writeFileSync(destFilePath, fileContent);
+        }
+    }
+}
+
+function _getDestFilePath(destDir, rootDir, filePath) {
+    return path.join(destDir, _getRelativePath(rootDir, filePath, true));
+}
+
+function _generateIndexFile(fileDataMap: IFileDataMap, rootDir, destDir) {
+    var indexFilePath = path.join(destDir, "index.ts");
+
+    if (fs.existsSync(indexFilePath)) {
+        throw new Error(`already exist index file:${indexFilePath}, can't generate it!`);
+    }
+
+    let content = "";
+
+    for (let filePath in fileDataMap) {
+        if (fileDataMap.hasOwnProperty(filePath)) {
+            let fileData: IFileData = fileDataMap[filePath],
+                nameArr = fileData.exportData.nameArr;
+
+            if (nameArr.length === 0) {
+                continue;
+            }
+
+            content += `export {${nameArr}} from "${_getRelativePath(rootDir, filePath, true)}";\n`;
+        }
+    }
+
+    fs.writeFileSync(indexFilePath, content);
+}
+
+function _addImport(importData: IImportData, fileContent: string) {
+    var importContent = "";
+
+    for (let filePath in importData) {
+        if (importData.hasOwnProperty(filePath)) {
+            let nameArr = importData[filePath];
+
+            importContent += `import {${nameArr.join(',')}} from "${filePath.replace(".ts", "")}";\n`;
+        }
+    }
+
+    if (importContent.length > 0) {
+        return `${importContent}\n${fileContent}`;
+    }
+
+    return fileContent;
+}
+
+
+function _isExported(node: ts.Node): boolean {
+    return (node.flags === ts.NodeFlags.Export)
+        || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
+}
+
+
+//todo move to wonder-package
+function parseOption(name) {
+    var value = null,
+        i = process.argv.indexOf(name);
+
+    if (i > -1) {
+        value = process.argv[i + 1];
+    }
+
+    return value;
+}
+
+// function isDefinOption(name) {
+//     return process.argv.indexOf(name) > -1;
+// }
+//
+// module.exports = {
+//     parseOption: parseOption,
+//     isDefinOption: isDefinOption
+// }
+
+
+var rootDir = parseOption("--rootDir"),
+    sourceFileGlobArr = parseOption("--sourceFileGlob").split(','),
+    destDir = parseOption("--destDir") || "./dest/";
+
+fs.removeSync(destDir);
+
+generateDocumentation(rootDir, sourceFileGlobArr, destDir, {
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.System
+});
