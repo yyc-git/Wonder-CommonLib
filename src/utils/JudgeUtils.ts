5c51e198c3f5a77e9d04a5b5805a6b54cf48f1f5
yyc <395976266@qq.com>
2017-02-18 08:37:20 +0800

    refactor: JudgeUtils:move MAX_ARRAY_INDEX into class

--- src/utils/JudgeUtils.ts
+++ src/utils/JudgeUtils.ts
@@ -1,9 +1,8 @@
 declare var global: any, module: any;
 
-const MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
-
 export class JudgeUtils {
     public static isArray(arr: any) {
+        const MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
         var length = arr && arr.length;
 
         return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
